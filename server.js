a76e4a1ac8bd359d0d5d30403d52ec29d031e600
Kirill <kirillsavorin@gmail.com>
2025-03-09 21:56:06 +0100

    CSP

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-// server.js
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-res.setHeader('Content-Security-Policy', "upgrade-insecure-requests;");
+
+
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "upgrade-insecure-requests;");
+  next();
+});
 
 let messages = [];
 
