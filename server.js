8fbcf5d22b3395f51c7a597e096fb07d0f764bd4
Kirill <kirillsavorin@gmail.com>
2025-03-09 23:15:12 +0100

    CSP

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
 }));
+
 
 let messages = [];
 
